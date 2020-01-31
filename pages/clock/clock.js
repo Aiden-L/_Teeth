02aca6f6fab9106a3b6b05f736cbf174389a3e29
Ruvik <1092045998@qq.com>
2020-01-31 11:22:26 +0800

    解决鬼畜

--- pages/clock/clock.js
+++ pages/clock/clock.js
@@ -41,6 +41,14 @@ Page({
     console.error(e.detail.errMsg)
   },
   but:function(e){
+    var flag=getApp().globalData.flag;
+    console.log(flag);
+    if(flag==true)
+    {
+      return;
+    }
+    getApp().globalData.flag=true;
+    console.log(flag);
     // 使用 wx.createContext 获取绘图上下文 context
     var context = wx.createCanvasContext('canvass');
     var n = 120;     // 倒计时圈中的文字显示
@@ -49,8 +57,8 @@ Page({
     function drawInnerCircle() {    // 绘制固定内圈圆
       context.save();              // save-保存绘图上下文
       context.setLineWidth(1);     // setLineWidth-设置线条宽度；参数-setLineWidth(线条宽度，单位px)
-      context.moveTo(200, 100);    // moveTo-把路径移动到画布中的制定点，不创建线条；参数-moveTo(目标位置的x坐标，目标位置的y坐标)
-      context.arc(100, 100, 100, 0, 2 * Math.PI, true);  // arc-创建一条弧线；参数-arc(圆心x坐标，圆心y坐标，圆半径，起始弧度，终止弧度，弧度方向是否是逆时针)
+      context.moveTo(180, 100);    // moveTo-把路径移动到画布中的制定点，不创建线条；参数-moveTo(目标位置的x坐标，目标位置的y坐标)
+      context.arc(100, 100, 80, 0, 2 * Math.PI, true);  // arc-创建一条弧线；参数-arc(圆心x坐标，圆心y坐标，圆半径，起始弧度，终止弧度，弧度方向是否是逆时针)
       context.stroke();            // stroke-画出当前路径的边框，默认颜色为黑色
       context.restore();           // restore-恢复之前保存的绘图上下文
       context.draw();    // draw-将之前在绘图上下文中的描述(路径，变形，样式)画到canvas中
@@ -58,8 +66,8 @@ Page({
     function drawOutCircle(n) {    // 绘制倒计时外圈圆
       context.save();
       context.setLineWidth(4)
-      context.moveTo(200, 100)
-      context.arc(100, 100, 100, 0, -Math.PI * 2 / 120 * n, true)
+      context.moveTo(180, 100)
+      context.arc(100, 100, 80, 0, -Math.PI * 2 / 120 * n, true)
       context.stroke()
       context.restore();
     }
