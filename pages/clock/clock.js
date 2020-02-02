9134c126a803cd93b5d20dbc0bfb8db874978033
sdl <1262671583@qq.com>
2020-02-02 09:37:32 +0800

    1
    1

--- pages/clock/clock.js
+++ pages/clock/clock.js
@@ -23,10 +23,10 @@ Page({
         "id": "1",
         "text": "普通牙刷"
       }],
-      poster: 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000',
-      name: '此时此刻',
-      author: '许巍',
-      src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
+    poster: 'http://p1.music.126.net/GlV130UzFe2hEofOpo9k2g==/109951163314358137.jpg?param=130y130',
+      name: '刷牙歌',
+      author: '宝宝巴士',
+      src: 'http://music.163.com/song/media/outer/url?id=566443217.mp3',
   },
     //事件处理函数 点击text
     toast: function() {
